9d3ac8eff98d85775d9cd88ee81ef696dd1da3f6
AlaaEldeep <>
2023-07-13 13:10:49 +0300

    style sidebar

--- src/components/Menu/page.tsx
+++ src/components/Menu/page.tsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 import styles from "./styles.module.scss";
 
 const Menu = () => {
-    return <div className={styles.test}>Menu</div>;
+    return (
+        <div className={styles.menu}>
+            {menu.map((item) => (
+                <div key={item.id} className={styles.item}>
+                    <span className={styles.title}>{item.title}</span>
+                    {item.listItems.map((item) => (
+                        <Link
+                            to={item.url}
+                            className={styles.listItem}
+                            key={item.id}
+                        >
+                            <img src={item.icon} alt="" />
+                            <span className={styles.listItemTitle}>
+                                {item.title}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Menu;
