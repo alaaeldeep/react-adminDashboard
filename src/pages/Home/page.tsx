daf5fca9220ef1d2c9ee9c889db24ccf33b24a2b
AlaaEldeep <>
2023-07-13 04:42:01 +0300

    init

---
+++ src/pages/Home/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+const Home = () => {
+    return <div className={styles.test}>Home</div>;
+};
+
+export default Home;
---
+++ src/pages/Users/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+const Users = () => {
+    return <div className={styles.test}>Users</div>;
+};
+
+export default Users;
